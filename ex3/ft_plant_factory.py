d90a1c327e7ac19456cdc8b9c0b0bd44c532e863
nkerstin <nkerstin@student.42heilbronn.de>
2026-07-13 16:37:17 +0200

    fighting with mypy

--- ex3/ft_plant_factory.py
+++ ex3/ft_plant_factory.py
@@ -2,19 +2,19 @@ class Plant:
     def __init__(self, name: str, height: float, age: int) -> None:
         self.name = name
         self.height = height
-        self.age = age
+        self._age = age
         self.growth: float = 0.0
 
     def show(self) -> str:
         return (f"{self.name}: {round(self.height, 1)}cm,"
-                f" {self.age} days old")
+                f" {self._age} days old")
 
     def grow(self, n: float) -> None:
         self.height = self.height + n
         self.growth = self.growth + n
 
     def age(self, n: int) -> None:
-        self.age = self.age + n
+        self._age = self._age + n
 
 
 if __name__ == "__main__":
